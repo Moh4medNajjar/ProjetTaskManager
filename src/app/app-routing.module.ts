f148994c9fba418c9f9c223a779ec03e55bd5506
Moh4medNajjar <najjarmohamed443@gmail.com>
2023-12-19 15:26:21 +0100

    basic input needs design improvements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './Auth/sign-in/sign-in.component';
+import { SignUpComponent } from './Auth/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth/sign-in', component: SignInComponent},
+  {path: 'auth/sign-up', component: SignUpComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './Auth/sign-in/sign-in.component';
 import { SignUpComponent } from './Auth/sign-up/sign-up.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SignUpComponent } from './Auth/sign-up/sign-up.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
